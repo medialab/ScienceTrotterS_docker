5360ff871aa4850fb711edb58e0e0f15ce8e4bfb
Paul Girard <paul.girard@sciencespo.fr>
2018-09-21 17:41:43 +0200

    new update_database.php init script

--- setup_php_db.sh
+++ setup_php_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo 'installing depedencies'
 docker-compose exec php composer install;
-echo 'init database with test data'
+echo 'init database...'
 docker-compose exec php php init_database.php;
+echo 'update database...'
+docker-compose exec php php update_database.php;
+echo 'done'
